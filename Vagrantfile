11d34f9e8c94420c10379bccb71162f140aee13a
Justin Hilles <jhilles@undergroundelephant.com>
2014-08-14 16:54:35 -0700

    init

---
+++ Vagrantfile
@@ -0,0 +1,25 @@
+hostname = "site.localhost"
+host_aliases = %w("")
+bootstrap_file = "bootstrap.sh"
+shared_folder = "../."
+public_folder = "/var/www/html/site"
+ip = "192.168.56.101"
+memory = 2048
+
+Vagrant.configure("2") do |config|
+    config.vm.box = "trusty64"
+    config.vm.box_url = "https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-i386-vagrant-disk1.box"
+    config.vm.network :private_network, ip: ip
+    config.vm.synced_folder shared_folder, public_folder
+    config.vm.host_name = hostname
+    config.hosts.aliases = host_aliases
+
+    config.vm.provider :virtualbox do |v|
+        v.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+        v.customize ["modifyvm", :id, "--memory", memory]
+        v.customize ["modifyvm", :id, "--name", hostname]
+        v.customize ["guestproperty", "set", :id, "/VirtualBox/GuestAdd/VBoxService/--timesync-set-threshold", 10000]
+    end
+
+    config.vm.provision :shell, :path => bootstrap_file, args: [hostname, public_folder, ip]
+end
---
+++ bootstrap.sh
@@ -0,0 +1,39 @@
+hostname = $1
+public_folder = $2
+ip = $3
+
+debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password 123'
+debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password 123'
+
+cat /vagrant/hosts >> /etc/hosts
+cat "$3 $1" >> /etc/hosts
+
+printf "Running Update"
+apt-get update > /dev/null 2>&1
+
+printf "Running Upgrade"
+apt-get upgrade > /dev/null 2>&1
+
+printf "Installing Packages"
+apt-get -y -q install python-software-properties build-essential apache2 mysql-server libapache2-mod-auth-mysql libapache2-mod-php5 php5 php5-mysql php5-mcrypt php5-memcached php5-curl memcached libmemcached-tools libmemcached-dev libcurl3 libcurl4-gnutls-dev curl vim wget git default-jre > /dev/null 2>&1
+curl -sS https://getcomposer.org/installer | php > /dev/null 2>&1 && sudo mv composer.phar /usr/local/bin/composer
+gem install sass > /dev/null 2>&1
+
+printf "Configuring Packages"
+cat /vagrant/database | mysql -uroot -p123
+sed -i '/display_errors = Off/c display_errors = On' /etc/php5/apache2/php.ini
+sed -i '/short_open_tag = Off/c short_open_tag = On' /etc/php5/apache2/php.ini
+sed -i '/short_open_tag = Off/c short_open_tag = On' /etc/php5/cli/php.ini
+sed -i '/error_reporting = E_ALL & ~E_DEPRECATED/c error_reporting = E_ALL | E_STRICT' /etc/php5/apache2/php.ini
+cp /vagrant/vhost /etc/apache2/sites-available/site.conf
+echo "User vagrant" >> /etc/apache2/apache2.conf
+echo "Group vagrant" >> /etc/apache2/apache2.conf
+
+printf "Enable Site"
+chown vagrant: /var/www
+a2enmod rewrite > /dev/null 2>&1
+a2ensite site.conf > /dev/null 2>&1
+a2dissite 000-default.conf > /dev/null 2>&1
+service apache2 reload > /dev/null 2>&1
+
+printf "Done!"
