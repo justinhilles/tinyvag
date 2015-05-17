f39b072a9eabbadf3f11e1066191e7313d41db93
Justin Hilles <jhilles@undergroundelephant.com>
2015-05-16 17:22:19 -0700

    refactor for yaml

--- Vagrantfile
+++ Vagrantfile
@@ -1,28 +1,33 @@
 $pwd = File.basename(File.expand_path(Dir.pwd))
-$hostname = $pwd << ".localhost"
-$bootstrap_file = "vendor/justinhilles/tinyvag/bootstrap.sh"
-$shared_folder = "./"
-$public_folder = "/var/www/html/site"
-$document_root = $public_folder
-$ip = "192.168.56.101"
-$memory = 2048
-$box = "ubuntu/trusty64"
+$cfg = 'app/vagrant.yml'
 
-if File.exist?("config.rb")
-    require "config.rb"
+data = {
+    'hostname'  => $pwd << '.dev',
+    'bootstrap' => 'bootstrap.sh',
+    'shared'    => './',
+    'public'    => '/var/www/html/site',
+    'mem'       => 2048,
+    'cpus'      => 4,
+    'box'       => 'ubuntu/trusty64'
+}
+
+if File.exist?($cfg)
+    require 'yaml'
+    data = data | YAML::load(File.open($cfg))
 end
 
-Vagrant.configure("2") do |config|
-    config.vm.box = $box
-    config.vm.network "private_network", type: "dhcp"
-    config.vm.synced_folder $shared_folder, $public_folder
-    config.vm.host_name = $hostname
+Vagrant.configure('2') do |config|
+    config.vm.box       = data['box']
+    config.vm.host_name = data['hostname']
+    config.vm.network 'private_network', type: 'dhcp'
+    config.vm.synced_folder data['shared'], data['public'], type: 'nfs'
 
     config.vm.provider :virtualbox do |v|
-        v.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
-        v.customize ["modifyvm", :id, "--memory", $memory]
-        v.customize ["modifyvm", :id, "--name", $hostname]
+        v.customize ['modifyvm', :id, '--natdnshostresolver1', 'on']
+        v.customize ['modifyvm', :id, '--name', data['hostname']]
+        v.customize ['modifyvm', :id, '--memory', data['mem']]
+        v.customize ['modifyvm', :id, '--cpus', data['cpus']]
     end
 
-    config.vm.provision :shell, :path => $bootstrap_file, args: [$hostname, $ip, $document_root]
+    config.vm.provision :shell, :path => data['bootstrap'], args: [data['hostname'], '127.0.0.1', data['public']]
 end
--- bootstrap.sh
+++ bootstrap.sh
@@ -5,21 +5,22 @@ debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again pa
 
 echo "$2 $1" >> /etc/hosts
 
-printf "Running Update"
+echo "Running Update"
 apt-get update
 
-printf "Running Upgrade"
+echo "Running Upgrade"
 apt-get upgrade
 
-printf "Installing Packages"
+echo "Installing Packages"
 apt-get -y -q install python-software-properties build-essential apache2 mysql-server libapache2-mod-auth-mysql libapache2-mod-php5 php5 php5-mysql php5-mcrypt php5-memcached php5-curl memcached libmemcached-tools libmemcached-dev libcurl3 libcurl4-gnutls-dev curl vim wget git default-jre
 curl -sS https://getcomposer.org/installer | php && sudo mv composer.phar /usr/local/bin/composer
 gem install sass
 
-printf "Configuring Packages"
+echo "Configuring Packages"
 mysql -uroot -p123 -e 'CREATE DATABASE IF NOT EXISTS `site`;CREATE USER `site`@`localhost` IDENTIFIED BY "123";GRANT ALL ON `site`.* TO `site`@`localhost`;FLUSH PRIVILEGES;'
 
 sed -i '/display_errors = Off/c display_errors = On' /etc/php5/apache2/php.ini
+sed -i '/display_errors = Off/c display_errors = On' /etc/php5/cli/php.ini
 sed -i '/short_open_tag = Off/c short_open_tag = On' /etc/php5/apache2/php.ini
 sed -i '/short_open_tag = Off/c short_open_tag = On' /etc/php5/cli/php.ini
 sed -i '/error_reporting = E_ALL & ~E_DEPRECATED/c error_reporting = E_ALL | E_STRICT' /etc/php5/apache2/php.ini
@@ -29,20 +30,8 @@ echo "<VirtualHost *:80>
     <Directory $3>
         Options Indexes FollowSymLinks MultiViews
         AllowOverride All
-        DirectoryIndex app_dev.php app.php index.php
+        Order allow,deny
         Allow from all
-        <IfModule mod_rewrite.c>
-            RewriteEngine On
-            RewriteCond %{REQUEST_URI}::\$1 ^(/.+)/(.*)::\2$
-            RewriteRule ^(.*) - [E=BASE:%1]
-            RewriteCond %{HTTP:Authorization} .
-            RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
-            RewriteCond %{ENV:REDIRECT_STATUS} ^$
-            RewriteRule ^app\.php(/(.*)|$) %{ENV:BASE}/\$2 [R=301,L]
-            RewriteCond %{REQUEST_FILENAME} -f
-            RewriteRule .? - [L]
-            RewriteRule .? %{ENV:BASE}/app.php [L]
-        </IfModule>
     </Directory>
     LogLevel debug
 </VirtualHost>" > /etc/apache2/sites-available/000-default.conf
@@ -50,9 +39,9 @@ echo "User vagrant" >> /etc/apache2/apache2.conf
 echo "Group vagrant" >> /etc/apache2/apache2.conf
 chown vagrant: /var/www
 
-printf "Enable Site"
+echo "Enable Site"
 a2enmod rewrite > /dev/null 2>&1
 a2ensite 000-default.conf > /dev/null 2>&1
 service apache2 reload > /dev/null 2>&1
 
-printf "Done!"
+echo "Done!"
